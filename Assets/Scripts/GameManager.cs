e6c39c81b5155bd79033abcc2f5d62980620016a
Dublinox <89817629+Dublinox@users.noreply.github.com>
2023-01-10 13:18:58 -0500

    TMPro & script additions
    TMPro & script additions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,8 +7,12 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
 
     public static GameManager Instance { get; private set; }
+    [Header("Links")]
+    [TextArea(1, 3)] public string gitHubURL = "https://github.com/ReynaldoReyes03";
+    [TextArea(1, 3)] public string projectRepositoryURL = "https://github.com/ReynaldoReyes03/Programming-Theory-Repository";
     private void Awake()
     {
+
         if (Instance != null)
         {
             Destroy(gameObject);
@@ -18,4 +22,5 @@ public class GameManager : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
     }
+
 }
---
+++ Assets/Scripts/Sphere.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// INHERITANCE
+
+public class Sphere : Shape
+{
+    private void Start()
+    {
+        ShapeName = transform.name;
+        ShapeLife = 5;
+    }
+
+    // POLYMORPHISM
+
+    protected override void OnMouseDown()
+    {
+        DisplayText("I am a sphere");
+    }
+}
